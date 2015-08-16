af00ad39cd2983644f7932c36d2c4d03df79262b
Cat <cat@Cats-Air.home>
2015-08-15 23:21:57 -0400

    check to show all

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -6,17 +6,18 @@ Players = new Mongo.Collection("players");
 if (Meteor.isClient) { 
   Template.leaderboard.helpers({
     players: function () {
-      if (Session.get("hideCompleted")) {
+       if (Session.get("showCompleted")) {
         // If hide completed is checked, filter tasks 
-        return Players.find({ type: { $exists: false }});
-        } 
-      else {
-        // Otherwise, return all of the tasks
         return Players.find({});
+        } 
+       else {
+        return Players.find({ type: { $exists: false }});
+        // Otherwise, return all of the tasks
+        //return Players.find({});
       }
     },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
+    showCompleted: function () {
+      return Session.get("showCompleted");
     },
     incompleteCount: function () {
         return Players.find({ type: { $exists: false }}).count();
@@ -34,8 +35,8 @@ if (Meteor.isClient) {
 
   Template.leaderboard.events({
 
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
+    "change .show-completed input": function (event) {
+      Session.set("showCompleted", event.target.checked);
     },
 
     'click .person': function () {
